fc0123b8062dfe4f0bf6c2459c0c773c89c7f4f5
jniedzie <jeremi.niedziela@cern.ch>
2020-09-22 16:10:41 +0200

    Cleaning up runAnalysis script

--- runAnalysis.py
+++ runAnalysis.py
@@ -1,13 +1,10 @@
 import autoencode.module.autoencodeSVJ.utils as utils
-import autoencode.module.autoencodeSVJ.trainer as trainer
 import autoencode.module.autoencodeSVJ.evaluate as ev
 import autoencode.module.autoencodeSVJ.summaryProcessor as summaryProcessor
 
 import pandas as pd
 import numpy as np
-import energyflow as ef
 import matplotlib.pyplot as plt
-import ROOT as rt
 
 import datetime
 import os
@@ -20,7 +17,7 @@ summary_path = output_path+"summary/"
 results_path = output_path+"trainingRuns/"
 
 input_path = "../data/training_data/"
-signal_path = input_path + "1500GeV_0.15/base_3/*.h5"
+signal_path = input_path + "2000GeV_0.75/base_3/*.h5"
 qcd_path = input_path + "qcd/base_3/*.h5"
 
 
@@ -59,8 +56,11 @@ def plotAuc():
     
     res = summary[pd.DatetimeIndex(summary.start_time) > datetime.datetime(year=2020, month=6, day=1, hour=22, minute=30)]
     res = res[res.epochs == 100]
+    
     plt.scatter(res.total_loss, res.mae_auc)
     plt.scatter(res.total_loss, res.mse_auc)
+    plt.xlabel("Total loss")
+    plt.ylabel("AUC (mae)")
     plt.show()
 
 
@@ -101,22 +101,22 @@ def printSummaries():
 # #help(ev.ae_train)
 #
 
-def trainWithRandomRate(n_trainings):
+def trainWithRandomRate(n_trainings, bottleneck_dim, batch_size):
     print("\nRunning trainWithRandomRate\n")
-    dim = 7
+
     mu, sigma = 0.00075, 0.01
 
     for i in range(n_trainings):
         print("\n\nStarting training ", i, "\n\n")
         learning_rate = np.random.lognormal(np.log(mu), sigma)
-        print('target_dim {}, learning_rate {}:'.format(dim, learning_rate),)
+        print('target_dim {}, learning_rate {}:'.format(bottleneck_dim, learning_rate),)
         auc = ev.ae_train(
             signal_path=signal_path,
             qcd_path=qcd_path,
             output_data_path=output_path,
-            target_dim=dim,
+            target_dim=bottleneck_dim,
             verbose=False,
-            batch_size=32,
+            batch_size=batch_size,
             learning_rate=learning_rate,
             norm_percentile=25
         )
@@ -159,19 +159,26 @@ def printTrainingInfo():
     print("Training info - val_loss: ", trainingInfo['metrics']['val_loss'][-1,1])
 
 
-def drawROCcurve(efp_base, bottleneck_dim, version=None):
+def getLatestSummaryFilePath(efp_base, bottleneck_dim, version=None):
     if version is None:
         version = summaryProcessor.get_last_summary_file_version(output_path, "hlf_eflow{}_{}_".format(efp_base, bottleneck_dim))
 
     input_summary_path = summary_path+"/hlf_eflow{}_{}_v{}.summary".format(efp_base, bottleneck_dim, version)
-    print("drawing ROC curve from summary file: ", input_summary_path)
+    return input_summary_path
+    
+
+def drawROCcurve(efp_base, bottleneck_dim, version=None):
+    input_summary_path = getLatestSummaryFilePath(efp_base, bottleneck_dim, version)
     elt = ev.ae_evaluation(input_summary_path)
     elt.roc(xscale='log')
 
 def addSampleAndPrintShape():
     newData = utils.data_loader(name="data")
-    newData.add_sample("data/background/constituents_3/data_0_data.h5")
-    print("shape: ", newData.data['jet_constituents'].shape)
+    newData.add_sample(input_path+"qcd/base_3/data_0_data.h5")
+    
+    print("shape: ", newData.data['jet_features'].shape)
+    
+    # print("shape: ", newData.data['jet_constituents'].shape)
 
 
 # def consts(globpath):
@@ -199,8 +206,10 @@ def addSampleAndPrintShape():
 #
 # data, jets = consts("TEST.h5")
 
-def plotJetFeatures():
-    evaluation = ev.ae_evaluation(summary_path + "/hlf_eflow3_7_v0.summary")
+def plotJetFeatures(efp_base, bottleneck_dim, version=None):
+    input_summary_path = getLatestSummaryFilePath(efp_base, bottleneck_dim, version)
+    
+    evaluation = ev.ae_evaluation(input_summary_path)
     main = evaluation.qcd
     
     for normer, rng in [
@@ -210,37 +219,43 @@ def plotJetFeatures():
     ]:
         var = main.cdrop('eflow *').norm(**normer)
         
-        #     rng = var.min().min(), var.max().max()
         plt.figure(figsize=(9, 9))
         for colname in var.axes[1]:
             plt.hist(var[colname], bins=70, range=rng, label=colname, histtype='step')
         plt.legend()
         plt.show()
 
+
+
+def loadAndPrintQCD():
+    data, jets, event, flavor = utils.load_all_data(qcd_path, name='QCD')
+    # print(help(ef.datasets.qg_jets.load))
+    
+    print("data: ", data)
+    print("jets: ", jets)
+    print("event: ", event)
+    print("flavor: ", flavor)
+
+
+
+
+### these work well:
+
+trainAndEvaluate(bottleneck_dim=8, batch_size=64, learning_rate=0.0005, epochs=100)
 # printSummary()
 # plotAuc()
+drawROCcurve(efp_base=3, bottleneck_dim=8)
+# addSampleAndPrintShape()
+# plotJetFeatures(efp_base=3, bottleneck_dim=8)
+# trainWithRandomRate(n_trainings=10, bottleneck_dim=8, batch_size=64)
+# loadAndPrintQCD()
 
-# trainWithRandomRate(10)
+### these don't work / I don't understand:
 
 # updataSignalEvals()
 
 
-# trainAndEvaluate(bottleneck_dim=8, batch_size=64, learning_rate=0.0005, epochs=100)
 
-drawROCcurve(efp_base=3, bottleneck_dim=8)
-
-
-
-# addSampleAndPrintShape()
-# plotJetFeatures()
-
-# data, jets, event, flavor = utils.load_all_data(qcd_path, name='QCD')
-# print(help(ef.datasets.qg_jets.load))
-
-# print("data: ", data)
-# print("jets: ", jets)
-# print("event: ", event)
-# print("flavor: ", flavor)
 
 
 
