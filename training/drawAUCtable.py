60e95be930304af1251ef1e3151d2d18f0b4d645
jniedzie <jeremi.niedziela@cern.ch>
2020-10-29 13:32:58 +0100

    Adding missing import

--- training/drawAUCtable.py
+++ training/drawAUCtable.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 # ------------------------------------------------------------------------------------------------
