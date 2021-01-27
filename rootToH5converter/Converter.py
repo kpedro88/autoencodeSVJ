10eef29c496ddb9f18119f0fbdc34a7a495edab4
jniedzie <jeremi.niedziela@cern.ch>
2021-01-27 16:36:52 +0100

    Refactoring constructor of the Converter class

--- rootToH5converter/Converter.py
+++ rootToH5converter/Converter.py
@@ -2,83 +2,56 @@ import uproot
 import numpy as np
 import energyflow as ef
 import os
-import sys
 import h5py
 from Jet import Jet
 from Event import Event
-import awkward as ak
+
 
 class Converter:
 
-    def __init__(
-        self,
-        input_path,
-        jet_delta_r=0.5,
-        max_n_constituents=100,
-        save_constituents=False,
-        energyflow_basis_degree=-1,
-    ):
+    def __init__(self, input_path, store_n_jets, jet_delta_r, max_n_constituents, EFP_degree):
         self.set_input_paths_and_selections(input_path=input_path)
 
-        # core tree, add files, add all trees
+        # read files, trees and recognize input type
         self.files = {path: uproot.open(path) for path in self.input_file_paths}
-
         self.trees = {}
         self.input_types = {}
-        
-        for path, file in self.files.items():
-            
-            for key in file.keys():
-                if key.startswith("Delphes"):
-                    self.trees[path] = file["Delphes"]
-                    self.input_types[path] = "Delphes"
-                    print("Adding Delphes tree")
-                elif key.startswith("Events"):
-                    self.trees[path] = file[key]
-
-                    if file[key]["JetPFCands_eta"] is not None:
-                        self.input_types[path] = "PFnanoAOD"
-                    else:
-                        self.input_types[path] = "nanoAOD"
-
-                    print("Adding nanoAOD tree: ", key)
-                else:
-                    print("Unknown tree type: ", key, ". Skipping...")
-        
-        
-        self.sizes = [tree.num_entries for tree in self.trees.values()]
-        self.nEvents = sum(self.sizes)
+        self.read_trees()
+        self.n_all_events = sum([tree.num_entries for tree in self.trees.values()])
+        self.n_events = sum(map(len, list(self.selections.values()))) + 1
 
         print("Found {0} file(s)".format(len(self.files)))
         print("Found {0} tree(s)".format(len(self.trees)))
-        print("Found {0} total event(s)".format(self.nEvents))
+        print("Found {0} total event(s)".format(self.n_all_events))
 
         
         self.jet_delta_r = jet_delta_r
-        self.n_jets = 2
-
         self.max_n_constituents = max_n_constituents
-        self.event_features = None
-        self.jet_features = None
-        self.jet_constituents = None
-        self.energy_flow_bases = None
-        self.save_constituents = save_constituents
-        self.efbn = energyflow_basis_degree
+        self.save_constituents = False if max_n_constituents < 0 else True
+        self.n_jets = store_n_jets
+        self.save_EFPs = False if EFP_degree < 0 else True
+        self.EFP_size = 0
 
-        if self.efbn < 0:
-            self.save_eflow = False
-            self.efbn = 0
-            self.efp_size = 0
-        else:
-            self.save_eflow = True
+        if EFP_degree >= 0:
             print("\n\n=======================================================")
-            print("Creating energyflow particle set with degree d <= {0}...".format(self.efbn))
-            self.efpset = ef.EFPSet("d<={0}".format(self.efbn), measure='hadr', beta=1.0, normed=True, verbose=True)
-            self.efp_size = self.efpset.count()
-            print("EFP set is size: {}".format(self.efp_size))
+            print("Creating energyflow particle set with degree d <= {0}...".format(EFP_degree))
+            self.efpset = ef.EFPSet("d<={0}".format(EFP_degree), measure='hadr', beta=1.0, normed=True, verbose=True)
+            self.EFP_size = self.efpset.count()
+            print("EFP set is size: {}".format(self.EFP_size))
             print("=======================================================\n\n")
+
+        print("Found ", self.n_events-1, " selected events, out of a total of ", self.n_all_events)
+
         
-        print("Found {0} selected events, out of a total of {1}".format(sum(map(len, list(self.selections.values()))), self.nEvents))
+        
+        self.event_features = np.empty((self.n_events, len(Event.get_features_names())))
+        self.jet_features = np.empty((self.n_events, self.n_jets, len(Jet.get_feature_names())))
+        self.jet_constituents = np.empty((self.n_events, self.n_jets, self.max_n_constituents, len(Jet.get_constituent_feature_names())))
+        self.energy_flow_bases = np.empty((self.n_events, self.n_jets, self.EFP_size))
+        
+        
+
+        
 
     def set_input_paths_and_selections(self, input_path):
         self.selections = {}
@@ -91,18 +64,30 @@ class Converter:
             self.input_file_paths.append(file_name)
             self.selections[file_name] = list(map(int, selections.split()))
 
+    def read_trees(self):
+        for path, file in self.files.items():
+        
+            for key in file.keys():
+                if key.startswith("Delphes"):
+                    self.trees[path] = file["Delphes"]
+                    self.input_types[path] = "Delphes"
+                    print("Adding Delphes tree")
+                elif key.startswith("Events"):
+                    self.trees[path] = file[key]
+                
+                    if file[key]["JetPFCands_eta"] is not None:
+                        self.input_types[path] = "PFnanoAOD"
+                    else:
+                        self.input_types[path] = "nanoAOD"
+                
+                    print("Adding nanoAOD tree: ", key)
+                else:
+                    print("Unknown tree type: ", key, ". Skipping...")
 
     def convert(self):
         
-        total_size = sum(map(len, list(self.selections.values()))) + 1
         total_count = 0
-
-        self.event_features = np.empty((total_size, len(Event.get_features_names())))
-        self.jet_features = np.empty((total_size, self.n_jets, len(Jet.get_feature_names())))
-        self.jet_constituents = np.empty((total_size, self.n_jets, self.max_n_constituents, len(Jet.get_constituent_feature_names())))
-        self.energy_flow_bases = np.empty((total_size, self.n_jets, self.efp_size))
         
-
         for file_name, tree in self.trees.items():
     
             print("\n\n=======================================================")
@@ -209,7 +194,7 @@ class Converter:
                     if self.save_constituents:
                         self.jet_constituents[total_count, iJet, :] = self.get_jet_constituents(jet.constituents)
 
-                    if self.save_eflow:
+                    if self.save_EFPs:
                         self.energy_flow_bases[total_count, iJet, :] = jet.get_EFPs(self.efpset)
                         
                 
@@ -219,7 +204,7 @@ class Converter:
             print("\n\n=======================================================")
 
         # remove redundant rows for events that didn't meet some criteria
-        for i in range(0, total_size-total_count):
+        for i in range(0, self.n_events - total_count):
             self.event_features = np.delete(self.event_features, -1, axis=0)
             self.jet_features = np.delete(self.jet_features, -1, axis=0)
             self.energy_flow_bases = np.delete(self.energy_flow_bases, -1, axis=0)
@@ -265,10 +250,10 @@ class Converter:
             jet_constituents.create_dataset('data', data=self.jet_constituents)
             jet_constituents.create_dataset('labels', data=Jet.get_constituent_feature_names())
     
-        if self.save_eflow:
+        if self.save_EFPs:
             eflow = f.create_group("jet_eflow_variables")
             eflow.create_dataset('data', data=self.energy_flow_bases)
-            eflow.create_dataset('labels', data=[str(i) for i in range(self.efp_size)])
+            eflow.create_dataset('labels', data=[str(i) for i in range(self.EFP_size)])
 
         f.close()
         print("Successfully saved!")
--- rootToH5converter/rootToH5.py
+++ rootToH5converter/rootToH5.py
@@ -9,14 +9,18 @@ parser.add_argument("-i", "--input", dest="input_path", default=None, required=T
 parser.add_argument("-o", "--output", dest="output_path", default="output.h5",
                     help="output file name (default: output.h5)")
 
-parser.add_argument("-c", "--constituents", dest='save_constituents', action='store_true',
-                    help="If specified, jte constituents will be stored in the output file.")
+parser.add_argument("-c", "--max_constituents", dest="max_constituents", type=int, default=-1,
+                    help="Maximum number of constituents per jet to be stored (default: not stored).")
 
-parser.add_argument("-m", "--max_constituents", dest="max_constituents", type=int, default=100,
-                    help="Maximum number of constituents per jet to be stored (default: 100).")
-
-parser.add_argument("-e", "--efp_basis_degree", dest="efp_basis_degree", type=int, default=-1,
+parser.add_argument("-e", "--EFP_degree", dest="EFP_degree", type=int, default=-1,
                     help="EFPs degree to be calculated and stored in the output file (default: not stored)")
+
+parser.add_argument("-r", "--delta_r", dest="delta_r", type=float, default=0.5,
+                    help="Delta R to assign constituents to jets. Only valid for Delphes, for PFnano links between jets and constituents are used")
+
+parser.add_argument("-j", "--store_n_jets", dest="store_n_jets", type=int, default=2,
+                    help="Number of jets to be stored (default: 2).")
+
 
 args = parser.parse_args()
 
@@ -25,14 +29,14 @@ print("\n\n=======================================================")
 print("Running ROOT to h5 converter with the following options: ")
 print("input: ", args.input_path)
 print("output: ", args.output_path)
-print("save constituents: ", args.save_constituents)
 print("max constituents: ", args.max_constituents)
-print("EFP basis degree: ", args.efp_basis_degree)
+print("EFP basis degree: ", args.EFP_degree)
 print("=======================================================\n\n")
 
 converter = Converter(input_path = args.input_path,
-                      save_constituents=args.save_constituents,
-                      energyflow_basis_degree=args.efp_basis_degree,
+                      store_n_jets= args.store_n_jets,
+                      jet_delta_r = args.delta_r,
+                      EFP_degree=args.EFP_degree,
                       max_n_constituents=args.max_constituents
                       )
 
